b4df12035eccac0338647e7c939d488d445cbda6
MakaylaClauson <makaylaclauson@gwu.edu>
2023-10-24 14:18:50 -0400

    add course attribute graph function

--- basicViz.py
+++ basicViz.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import requests, openpyxl
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('pgadata.xlsx')
+#df = pd.read_excel('pgadata.xlsx')
 
 
 def score_graph(tournament):
@@ -15,7 +15,7 @@ def score_graph(tournament):
     filtered_df = filtered_df[filtered_df['Name'].isin(selected_golfers)]
     print(filtered_df)
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(30, 6))
     sns.set(style="whitegrid")
     graph = sns.relplot(x="Year", y="Scoring Average", data=filtered_df, hue="Name", kind = "line")
     graph.set_titles("Scores Over Years by {tournament}")
@@ -23,4 +23,17 @@ def score_graph(tournament):
     #graph.fig.suptitle("Scoring Average Over Years by Tournament for Each Player")
     plt.show()
 
-score_graph("U.S. Open")
+#score_graph("U.S. Open")
+
+d = pd.read_csv("CourseTable.csv")
+
+def course_graph(attribute):
+    plt.figure(figsize=(15, 6))  # Adjust the figure size as needed
+    plt.bar(d.COURSE, attribute, color='skyblue')
+    plt.xlabel('Golf Course')
+    plt.xticks(rotation=45, fontsize=4)
+    plt.tight_layout()
+    plt.show()
+
+course_graph(d.yardage)
+course_graph(d.fw_width)
