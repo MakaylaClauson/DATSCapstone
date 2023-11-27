d0d9f51254a1b66443f23cabe6f84fb46b3deb92
MakaylaClauson <makaylaclauson@gwu.edu>
2023-11-27 12:26:27 -0500

    Cleaned Data PY aggregated

--- DataClean.py
+++ DataClean.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 #Need to subset by course to find average score per  par 3,4,5 
 #split the group into short,avg,long for each section
 #Add Categorical Course Attributes to be apply to subset data for precisely for model
@@ -6,6 +8,7 @@ holeData = pd.read_excel('HoleData.xlsx')
 #Join Course Data all together
 courseData = pd.read_excel("course_attributes.xlsx")
 courseSchedule = pd.read_excel('courseSchedule.xlsx')
+scores = pd.read_excel('dataset2.xlsx')
 
 courseList = holeData["Course"].unique()
 years = holeData["Year"].unique()
@@ -57,14 +60,40 @@ course = pd.merge(data, courseData, on=['Year','Course'], how='left')
 print(course.columns)
 course = pd.merge(course, courseSchedule, on=['Year','Course'], how='left')
 print(course.dtypes)
-#course.to_excel('PGACourseData.xlsx')
 
-#del course['COURSE']
-#print(course.dtypes)
-#print(course)
-#course.to_excel("CourseData.xlsx",index=False)
+def convert_length(length_str):
+    # Split the string into value and unit
+    if isinstance(length_str, str) == False:
+        length_str = str(length_str)
+        print(length_str)
+    if "'" in length_str and '"' in length_str:
+        inch, cent = length_str.split("'")
+        inch = inch.replace("'", "")
+        cent = cent.replace('"',"")
+        inch = float(inch)
+        cent = float(cent)* 0.393701
+        value = inch + cent
+    else:
+        value=float(length_str)
+    return value
 
-#Join Course Data and Player Data -- Research better Course Data 
-#data = pd.merge(scores, course,on=['Year','Tournament'], how = 'left')
-#print(data)
-#data.to_excel('PGA5_Dataset.xlsx')
+course[['Year', 'Tournament', 'Course']] = course[['Year', 'Tournament', 'Course']].astype(str)
+scores[['Year', 'Tournament']] = scores[['Year', 'Tournament']].astype(str)
+course.Tournament = course.Tournament.str.replace('\xa0','')
+
+scores['Rough Proximity'] = scores['Rough Proximity'].apply(convert_length)
+scores['Average Distance of Putts made'] = scores['Average Distance of Putts made'].apply(convert_length)
+scores['Proximity to Hole'] = scores['Proximity to Hole'].apply(convert_length)
+scores = pd.merge(scores, course, on=['Year','Tournament'], how='left')
+
+numerical_pipe = SimpleImputer(strategy ="mean")
+
+scores[['Total Putting', 'Driving Accuracy Percentage', 'Driving Distance',
+               'Greens in Regulation Percentage',  'Scrambling', 
+               'Par 3 Scoring Average', 'Par 4 Scoring Average', 'Par 5 Scoring Average']] = scores[['Total Putting', 'Driving Accuracy Percentage', 'Driving Distance',
+               'Greens in Regulation Percentage', 'Scrambling', 
+               'Par 3 Scoring Average', 'Par 4 Scoring Average', 'Par 5 Scoring Average']].apply(pd.to_numeric, errors='coerce')
+numeric_columns = scores.select_dtypes(include = ["number", "integer","float"]).columns
+scores[numeric_columns] = numerical_pipe.fit_transform(scores[numeric_columns])
+
+scores.to_excel("CleanedData.xlsx")
